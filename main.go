d7ad7e8d6247bb71a304b52758da4b91a08141b2
d3lvx <m4j3r@poton.me>
2026-03-03 00:39:32 -0500

    Created basic structure for REPL, added length condition

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// TODO: Create support for REPL
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")                 // CLI beginning
+		scanner.Scan()                          // Waits for user input
+		userInput := cleanInput(scanner.Text()) // Captures user input and cleans it
+
+		if len(userInput) > 0 {
+			fmt.Println("Your command was:", userInput[0]) // Prints first word user submitted
+		}
+	}
+
 }
