128b8f13654eaa8ab0b42d33f2249c71db7cf3e5
d3lvx <m4j3r@poton.me>
2026-03-03 23:13:57 -0500

    Added functionality to query to location area from PokeAPI, commandMap().

--- commands.go
+++ commands.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -27,6 +29,11 @@ func getCommands() map[string]cliCommand {
 			description: "Displays the help menu",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the map 20 locations at a time",
+			callback:    commandMap,
+		},
 	}
 }
 
@@ -49,5 +56,45 @@ func commandHelp() error {
 	}
 
 	fmt.Println("-----------------------")
+	return nil
+}
+
+// Map Function
+func commandMap() error {
+
+	// Create the struct for the Area JSON info
+	type Area struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	}
+
+	// Create the struct for the entire response
+	type Results struct {
+		Count    int    `json:"count"`
+		Next     string `json:"next"`
+		Previous string `json:"previous"`
+		Results  []Area `json:"results"`
+	}
+
+	url := "https://pokeapi.co/api/v2/location-area/"
+
+	res, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("An issue was encountered reaching the URL: %s", url)
+	}
+
+	defer res.Body.Close()
+
+	var results Results
+
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&results); err != nil {
+		return fmt.Errorf("Error decoding response body")
+	}
+
+	for _, area := range results.Results {
+		fmt.Println(area.Name)
+	}
+
 	return nil
 }
